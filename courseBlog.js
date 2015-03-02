449170732de619364e32fb31c1290e443e04037c
alexf388 <alexf388@hotmail.com>
2015-03-01 19:04:32 -0500

    Second commit with actual files from step 9

--- courseBlog.js
+++ courseBlog.js
@@ -2,3 +2,12 @@
  * Created by Abdelkader on 2015-01-31.
  */
 CourseBlog = Ember.Application.create();
+
+CourseBlog.ApplicationSerializer = DS.LSSerializer.extend();
+CourseBlog.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'CourseBlog'
+});
+
+
+
+
--- router.js
+++ router.js
@@ -4,5 +4,11 @@
 CourseBlog.Router.map(function() {
     this.resource('posts', {path: '/'});
     this.resource('about');
-    this.resource('contact');
+    this.resource('contact', function(){
+        this.resource('phone');
+        this.resource('email');
+    });
+    this.resource('post', {path: 'posts/:post_id'});
+    this.resource('addNewPost');
+
 });
