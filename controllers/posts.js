1c0062a5b510616861de52c698c974860d4e8fde
alexf388 <alexf388@hotmail.com>
2015-03-08 04:53:40 -0400

    image of company changes if you press on company name

---
+++ controllers/posts.js
@@ -0,0 +1,19 @@
+/**
+ * Created by alex on 07/03/15.
+ */
+CourseBlog.PostsController = Ember.ObjectController.extend({
+    companyChosen : false,
+    imageString: '',
+
+    actions:{
+        showThisShit:function(imageName){
+                this.set('companyChosen', true);
+                //var shortNameTemp = shortName;
+                //var imageFormat = ".png";
+                //var result = shortNameTemp.concat(imageFormat);
+                var result = imageName;
+                //console.log("shortName: ", shortName);
+                this.set('imageString', result);
+        }
+    }
+});
