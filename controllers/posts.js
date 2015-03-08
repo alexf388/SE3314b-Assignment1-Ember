ef146176b2d7eb2157ce0390f6e54656c9626f24
alexf388 <alexf388@hotmail.com>
2015-03-08 19:22:30 -0400

    More merging

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,23 +5,11 @@
 StockMarket.PostsController = Ember.ObjectController.extend({
     companyChosen : false,
     imageString: '',
-<<<<<<< HEAD
+
     PositiveNet: false,
     NegativeNet: false,
     NeutralNet: false,
 
-    actions:{
-        showThisShit:function(imageName){
-                this.set('companyChosen', true);
-                //var shortNameTemp = shortName;
-                //var imageFormat = ".png";
-                //var result = shortNameTemp.concat(imageFormat);
-                var result = imageName;
-                //console.log("shortName: ", shortName);
-                this.set('imageString', result);
-        }
-
-=======
     actions: {
         showThisShit: function (imageName) {
             this.set('companyChosen', true);
@@ -37,6 +25,5 @@ StockMarket.PostsController = Ember.ObjectController.extend({
             this.transitionToRoute('placeBidOrder',{queryParams:{company:"astring"}});
         }   
 
->>>>>>> e057143e16bd3fbf5bc519e433aa0f2306c289e2
     }
 });
