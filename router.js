c3ab1e5ebfa4b580362c225660b018d83f3fdfc5
David Kong <dk.kong94@gmail.com>
2015-03-08 16:17:28 -0400

    made changes, fixed formatting

--- router.js
+++ router.js
@@ -3,12 +3,8 @@
  */
 CourseBlog.Router.map(function() {
     this.resource('posts', {path: '/'});
-    this.resource('about');
-    this.resource('contact', function(){
-        this.resource('phone');
-        this.resource('email');
-    });
     this.resource('post', {path: 'posts/:post_id'});
+    this.resource('addNewCompany');
     this.resource('addNewPost');
 
 
