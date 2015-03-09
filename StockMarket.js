603fd1b76450110f01ab2c24e8cd9b2c86c54ab8
alexf388 <alexf388@hotmail.com>
2015-03-09 15:54:16 -0400

    Starte on marketByOrder with nested templates

--- StockMarket.js
+++ StockMarket.js
@@ -5,8 +5,7 @@ StockMarket = Ember.Application.create();
 
 StockMarket.ApplicationSerializer = DS.LSSerializer.extend();
 StockMarket.ApplicationAdapter = DS.LSAdapter.extend({
-    namespace: 'StockMarket',
-    namespace: 'Bids'
+    namespace: 'StockMarket'
 });
 
 StockMarket.PostAdapter = DS.FixtureAdapter.extend();
--- router.js
+++ router.js
@@ -2,9 +2,24 @@
  * Created by Abdelkader on 2015-01-31.
  */
 StockMarket.Router.map(function() {
-    this.resource('posts', {path: '/'});
+    //this.resource('posts', {path: '/'});
     this.resource('addNewCompany');
     this.resource('addNewPost');
     this.resource('placeBidOrder',{path:'/buy/:post_id'});
     this.resource('placeSellOrder',{path:'/sell/:post_id'});
+
+
+
+     this.resource("posts", {path: '/'}, function(){
+        //this.resource("marketByOrder");
+        //this.route("comment", { path: "/comments/:comment_id" });
+         this.route("marketByOrder");
+
+         //insert route marketByPrice
+
+     });
+
+
+
+
 });
