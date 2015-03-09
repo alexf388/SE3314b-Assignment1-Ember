c37b44b7ed6fa040be539f525cd250423354dae1
alexf388 <alexf388@hotmail.com>
2015-03-08 20:43:27 -0400

    Added buttons for sorting...the sorting doesn't work perfectly yet

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,13 +2,42 @@
  * Created by alex on 07/03/15.
  */
 
-StockMarket.PostsController = Ember.ObjectController.extend({
+StockMarket.PostsController = Ember.ArrayController.extend({
     companyChosen : false,
     imageString: '',
 
     PositiveNet: false,
     NegativeNet: false,
     NeutralNet: false,
+
+
+    //used for sorting table
+    sortProperties: ['id'],
+    theFilter: "",
+
+    checkFilterMatch: function(theObject, str) {
+        var field, match;
+        match = false;
+        for (field in theObject) {
+            if (theObject[field].toString().slice(0, str.length) === str) {
+                match = true;
+            }
+        }
+        return match;
+    },
+
+    filterPeople: (function() {
+        return this.get("arrangedContent").filter((function(_this) {
+            return function(theObject, index, enumerable) {
+                if (_this.get("theFilter")) {
+                    return _this.checkFilterMatch(theObject, _this.get("theFilter"));
+                } else {
+                    return true;
+                }
+            };
+        })(this));
+    }).property("theFilter", "sortProperties"),
+    //end: used for sorting table
 
     actions: {
         showThisShit: function (imageName) {
@@ -23,6 +52,10 @@ StockMarket.PostsController = Ember.ObjectController.extend({
         buyStock: function (company) {
             console.log(company);
             this.transitionToRoute('placeBidOrder',{queryParams:{company:"astring"}});
+        },
+        //sort table action
+        sortBy: function(property) {
+            this.set("sortProperties", [property]);
         }
 
     }
