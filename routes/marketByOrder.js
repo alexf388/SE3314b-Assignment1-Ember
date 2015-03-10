d5f455807d6bb0a05d8d47c59484297fa325bcb9
alexf388 <alexf388@hotmail.com>
2015-03-10 05:04:22 -0400

    Made a few changes here and there

--- routes/marketByOrder.js
+++ routes/marketByOrder.js
@@ -2,15 +2,15 @@
  * Created by alex on 09/03/15.
  */
 StockMarket.PostsMarketByOrderRoute = Ember.Route.extend({
-    model: function() {
+    model: function(params) {
         console.log("This course sucks");
         var store = this.store;
         return Ember.RSVP.hash({
             buys: store.find('buy').then(function(buys){
-                return buys.filterBy('companyid','1');
+                return buys.filterBy('companyid', params.post_id);
             }),
             sells: store.find('sell').then(function(sells){
-                return sells.filterBy('companyid','2');
+                return sells.filterBy('companyid',params.post_id);
             })
         });
     }
