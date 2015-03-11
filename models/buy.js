32e792f9ad52b07a02ce95ab40e3f6f67bc69180
David Kong <dk.kong94@gmail.com>
2015-03-10 22:03:36 -0400

    relationships now two way.God that was a stupid inclusion of the wrong ember-data version

--- models/buy.js
+++ models/buy.js
@@ -7,28 +7,3 @@ StockMarket.Buy = DS.Model.extend({
     price: DS.attr('')
 
 });
-
-StockMarket.Buy.FIXTURES = [
-    {
-        company:1,
-        volume: 10000,
-        price: 3.00
-    },
-    {
-        company:1,
-        volume: 9999,
-        price: 3.01
-    },
-    {
-        company:1,
-        volume: 3000,
-        price: 5.00
-    },
-    {
-        company:1,
-        volume: 10000,
-        price: 3.00
-    }
-
-
-];
